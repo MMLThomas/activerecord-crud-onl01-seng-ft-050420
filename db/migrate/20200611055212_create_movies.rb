7c2298770194745c7fe7e6df0b84a062f95bfe37
michael thomas <michael.thomas2014@gmail.com>
2020-06-11 05:52:54 +0000

    Automatically backed up by Learn

--- db/migrate/20200611055212_create_movies.rb
+++ db/migrate/20200611055212_create_movies.rb
@@ -1,5 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    rake db:create_migration NAME=create_movies
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date 
+      t.string :director
+      t.string :lead 
+      t.boolean :in_theaters
+    end
   end
 end
