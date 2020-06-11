0c8f143fcf7264ddd7369c14ad4587b76e035083
michael thomas <michael.thomas2014@gmail.com>
2020-06-11 07:31:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,8 +67,8 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  movie = Movie.find_by(title: "Awesome Flick").update(title: "Even Awesomer Flick")
-  movie.save
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
   movie
 end
 
@@ -84,7 +84,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.save
+  Movie.save--------
 end
 
 def can_destroy_a_single_item
